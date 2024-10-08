abce7f7af06e2e15e64b079fe69823931a674505
Лебедь Виолетта <wiolettalebed5@gmail.com>
2024-10-08 14:32:47 +0300

    language

--- hello.pas
+++ hello.pas
@@ -1,3 +1,10 @@
-﻿begin
-print('hello world')
+﻿program task10;
+begin  
+  var hello:= readstring('Введите язык, на котором вы говорите, Enter the language, Sprache eingeben');
+  if hello = ('русский язык') then
+    println('Привет');
+  if hello = ('English') then
+    println('Hello');
+  if hello = ('Deutsch') then
+    println('Hallo');
 end.
